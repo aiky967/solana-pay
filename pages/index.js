55599e638d263c47e76704d825257b5af66b1d33
aiky967 <kesharwania967@gmail.com>
2022-06-18 19:25:47 +0530

    Developed all the code

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import Product from "../components/Product";
 import HeadComponent from '../components/Head';
 
+import { useWallet } from '@solana/wallet-adapter-react';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import CreateProduct from '../components/createProduct';
+
 // Constants
-const TWITTER_HANDLE = "_buildspace";
+const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  
-  
+  const { publicKey } = useWallet();
+  const isOwner = ( publicKey ? publicKey.toString() === process.env.NEXT_PUBLIC_OWNER_PUBLIC_KEY : false );
+  const [creating, setCreating] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    if (publicKey) {
+      fetch(`/api/fetchProducts`)
+        .then(response => response.json())
+        .then(data => {
+          setProducts(data);
+          console.log("Products", data);
+        });
+    }
+  }, [publicKey]);
+
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img src="https://c.tenor.com/Rhec0g8u3bYAAAAd/anime-luffy.gif" alt="3rd gear" />
+
+      <div className="button-container">
+        <WalletMultiButton className="cta-button connect-wallet-button" /> 
+      </div>
+    </div>
+  );
+
+  const renderItemBuyContainer = () => (
+    <div className="products-container">
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  );
+
   return (
     <div className="App">
-      <HeadComponent/>
+      
       <div className="container">
         <header className="header-container">
-          <p className="header"> 😳 Buildspace Emoji Store 😈</p>
-          <p className="sub-text">The only emoji store that accepts sh*tcoins</p>
+          <p className="header"> 😳 One Piece Store 😈</p>
+          <p className="sub-text">The only anime store that accepts shitcoins</p>
+
+          {isOwner && (
+            <button className='create-product-button' onClick={() => setCreating(!creating)}>
+              {creating ? "Close" : "Create Product"}
+            </button>
+          )}
         </header>
 
         <main>
-          <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+          {/* <img src="https://i.kym-cdn.com/photos/images/newsfeed/000/513/534/f0e.gif" alt="hot-nami" /> */}
+          {creating && <CreateProduct />}
+          {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
         </main>
 
         <div className="footer-container">
